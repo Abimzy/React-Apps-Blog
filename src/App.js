170a99962d56b8480bed68b57a2384f15f60aba1
Abimzy <eabiemo@gmail.com>
2019-02-24 10:55:09 -0800

    Created Hello component with props for name, last, age. Displayed on virtual DOM

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
+//Bring in React and Component instance from react
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
+//Define our hello component
+class Hello extends Component {
+  //What should the component render
+  render (){
+    //Make sure to return some UI
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <h1>Hello {this.props.name} {this.props.last}</h1>
+      <p>You are {this.props.age} years young beautiful!</p>
       </div>
-    );
+    )
   }
 }
+export default Hello
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import logo from './logo.svg';
+//import './App.css'; 
+//class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Hello from './App.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+//ReactDOM.render takes 2 arguments, the component (in this case Hello) and the DOM element (in this case entire document with id of 'root')
+ReactDOM.render (
+    <Hello name={"Eunice"} last={"Abimzy"} age={30}/>,
+    document.getElementById('root')
+)
+
+
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
