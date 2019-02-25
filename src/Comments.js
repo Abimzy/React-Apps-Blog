68a209ebecee38e361d0c7ec13efa493abb1b156
Abimzy <eabiemo@gmail.com>
2019-02-24 17:05:34 -0800

    Created Comment component to make listing of comments DRY

---
+++ src/Comments.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Comment extends Component {
+    render (){
+        return (
+            <div>
+                <p>{this.props.message}</p>
+            </div>
+        )
+    }
+}
+export default Comment
